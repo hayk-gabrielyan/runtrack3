ac851541ac3d292a48f206db49c96f38585e8b42
Hayk Gabrielyan <hayk.gabrielyan@laplateforme.io>
2023-01-16 16:40:09 +0100

    runtrack3-Javascript - Job09 - function qui tri des nombres

---
+++ jour01/job09/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Somme des nombres premiers</title>
+    <script src="script.js"></script>
+</head>
+<body>
+
+</body> 
+</html>
---
+++ jour01/job09/script.js
@@ -0,0 +1,19 @@
+var numbers = [58,99,29,37,5,90,63,72,81,12];
+var list = document.getElementById("number-list");
+console.log("voici la liste des nombres : ")
+console.log(numbers);
+
+function sort(numbers, order) {
+    if (order === "asc") {
+        numbers.sort(function(a, b) {
+            return a - b;
+        });
+    } else if (order === "desc") {
+        numbers.sort(function(a, b) {
+            return b - a;
+        });
+    }
+    console.log(numbers);
+}
+console.log("voici la même liste dans l'ordre choisi : ")
+sort(numbers, "asc");
