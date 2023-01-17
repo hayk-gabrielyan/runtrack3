7182607b5b2cd7d8250f005e052d26c59ffa5607
Hayk Gabrielyan <hayk.gabrielyan@laplateforme.io>
2023-01-17 23:38:25 +0100

    runtrack3-Javascript Jour2 - Job04 - footer qui change du couleur en function de pourcentage de scroll sur la page

---
+++ jour02/job05/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Jour02 - Job01</title>
+    <link rel="stylesheet" href="style.css" />
+    <script src="script.js"></script>
+</head>
+<body>
+    <footer id="footer"></footer>
+</body>
+</html>
